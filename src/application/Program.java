bf7f471b335e9648bde9c1f71fda2397af172ece
Igor <igorfaria@discente.ufg.br>
2021-04-30 17:14:15 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,18 +30,21 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		/*
 		System.out.println("\n=======TEST 4: seller insert========"); 
 		Seller seller2 = new Seller(null, "Greg", "Gre@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(seller2);
 		System.out.println("Inserted! new id: "+ seller2.getId());
-		
+		*/
 		System.out.println("\n=======TEST 5: seller update========"); 
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
+		System.out.println("\n=======TEST 6: seller delete========"); 
+		sellerDao.deleteById(9);
+		System.out.println("Delete Completed");
 		
 	}
 
